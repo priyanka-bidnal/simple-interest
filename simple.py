cc2a50d492899e79edb07aece4df6a0dd5773318
priyanka-bidnal <bidnalpriyanka@gmail.com>
2025-10-23 14:32:56 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -3,5 +3,8 @@ R = float(input("Enter Rate of Interest (%): "))
 T = float(input("Enter Time (in years): "))
 
 SI = (P * R * T) / 100
+CI = P * ((1 + R / 100) ** T) - P
 
 print(f"Simple Interest: ₹{SI}")
+
+print(f"Compound Interest: ₹{CI}")
